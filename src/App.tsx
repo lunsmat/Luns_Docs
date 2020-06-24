9406794125a763e4427c842ec6319364e9760569
Luan Farias <matheus.frs22123@outlook.com>
2020-06-23 21:06:51 -0300

    Global style was maden

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
+import Routes from './routes';
 
-function App() {
-    return (
-        <div />
-    );
-}
+const App: React.FC = () => <Routes />;
 
 export default App;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import GlobalStyle from './styles/global';
+
 import Home from './pages/Home';
 
 const Routes: React.FC = () => {
     return (
         <BrowserRouter>
+            <GlobalStyle />
             <Switch>
                 <Route exact path="/" component={Home} />
             </Switch>
