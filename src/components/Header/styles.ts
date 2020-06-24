39bf609adda8af270a53004c81ff0e4f9c172744
Luan Farias <matheus.frs22123@outlook.com>
2020-06-23 21:16:09 -0300

    Header was maden

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+    height: 80px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #6f42c1;
+    color: #FFF;
+`;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import GlobalStyle from './styles/global';
+import Header from './components/Header';
 
 import Home from './pages/Home';
 
@@ -9,6 +10,7 @@ const Routes: React.FC = () => {
     return (
         <BrowserRouter>
             <GlobalStyle />
+            <Header />
             <Switch>
                 <Route exact path="/" component={Home} />
             </Switch>
